6dc1ffc4659af1ae50160361ec0cbfb2e90c5e89
Peter Shipton <Peter_Shipton@ca.ibm.com>
2026-04-07 13:45:58 -0400

    Use the openssl-3.5.6 release now it's available
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -423,7 +423,7 @@ updateOpenj9Sources() {
     fi
 
     # NOTE: fetched openssl will NOT be used in the RISC-V cross-compile situation
-    bash get_source.sh -openssl-branch=openssl-3.5 ${OPENJCEPLUS_FLAGS} ${GSKIT_FLAGS} ${GSKIT_CREDENTIALS}
+    bash get_source.sh -openssl-branch=openssl-3.5.6 ${OPENJCEPLUS_FLAGS} ${GSKIT_FLAGS} ${GSKIT_CREDENTIALS}
     cd "${BUILD_CONFIG[WORKSPACE_DIR]}"
   fi
 }
