45e2086bfc7012ed425983158e0ef23fa30e9176
Peter Shipton <Peter_Shipton@ca.ibm.com>
2026-06-10 13:45:15 -0400

    Update OpenSSL to 3.5.7
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- sbin/prepareWorkspace.sh
+++ sbin/prepareWorkspace.sh
@@ -419,7 +419,7 @@ updateOpenj9Sources() {
     fi
 
     # NOTE: fetched openssl will NOT be used in the RISC-V cross-compile situation
-    bash get_source.sh -openssl-branch=openssl-3.5.6 ${OPENJCEPLUS_FLAGS} ${GSKIT_FLAGS} ${GSKIT_CREDENTIALS}
+    bash get_source.sh -openssl-branch=openssl-3.5.7 ${OPENJCEPLUS_FLAGS} ${GSKIT_FLAGS} ${GSKIT_CREDENTIALS}
     cd "${BUILD_CONFIG[WORKSPACE_DIR]}"
   fi
 }
